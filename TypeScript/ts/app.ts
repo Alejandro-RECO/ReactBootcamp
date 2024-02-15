44eb3fe7ff9a8cea3e9f0128ba91eb91bd163705
Alejandro-RECO <130815506+Alejandro-RECO@users.noreply.github.com>
2024-02-14 22:07:20 -0500

    TypeScript Learn

---
+++ TypeScript/ts/app.ts
@@ -0,0 +1,20 @@
+const obj = { width: 10, height: 15 };
+const area = obj.width * obj.height;
+
+let a = (1) 
+
+
+//console.log(4 / []);//TypeScript considera que la división de un número por una matriz es una operación sin sentido y generará un error
+
+let helloworld = "hello word";
+
+interface User{
+  name: string,
+  id: number,
+}
+
+const user: User = {
+  //username: "user",//Si proporciona un objeto que no coincide con la interfaz que proporcionó, TypeScript le advertirá
+  name: "Hayes",
+  id: 0,
+}
